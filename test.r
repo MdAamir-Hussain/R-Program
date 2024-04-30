f0901a250bb3562eea18326b96d71e26cc96e100
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2024-04-30 17:14:28 +0530

    firstcommit

---
+++ test.r
@@ -0,0 +1,15 @@
+# Define a list of numbers
+numbers <- c(2, 4, 6, 8, 10)
+
+# Calculate the sum of the numbers
+total <- sum(numbers)
+
+# Calculate the length of the list
+count <- length(numbers)
+
+# Calculate the average
+average <- total / count
+
+# Print the average
+print(average)
+print('hello r program in VS CODE!!!')
